deac47df46e2d71b9dc03f4532d11ad1c33a74ed
daniuk <wykretbastard@o2.pl>
2015-11-03 12:50:23 +0100

    more tests

--- tests/test_idcollector.py
+++ tests/test_idcollector.py
@@ -1,26 +1,39 @@
+import requests
+
 from imdb.idcollector import IdCollector
 
-def test_parse_html_for_links():
-    instance = IdCollector()
-    links = instance._parse_html_for_links()
-    assert len(links) == 250
 
-def test_get_ids_100():
-    instance = IdCollector()
-    instance.get_ids(100)
-    assert len(instance.ids_list) == 100
+class TestIdCollector(object):
 
-def test_get_ids_10():
-    instance = IdCollector()
-    instance.get_ids(10)
-    assert len(instance.ids_list) == 10
+    def test_init(self):
+        id_collector = IdCollector()
+        assert id_collector.ids_list == []
+        assert id_collector.html.status_code == 200 
 
-def test_get_ids_200():
-    instance = IdCollector()
-    instance.get_ids(200)
-    assert len(instance.ids_list) == 200
+    def test_parse_html_for_links(self):
+        instance = IdCollector()
+        links = instance._parse_html_for_links()
+        assert len(links) == 250
 
-def test_get_ids_300():
-    instance = IdCollector()
-    instance.get_ids(300)
-    assert len(instance.ids_list) == 250
+    def test_get_ids_100(self):
+        instance = IdCollector()
+        instance.get_ids(100)
+        assert len(instance.ids_list) == 100
+
+    def test_get_ids_10(self):
+        instance = IdCollector()
+        instance.get_ids(10)
+        assert len(instance.ids_list) == 10
+
+    def test_get_ids_200(self):
+        instance = IdCollector()
+        instance.get_ids(200)
+        assert len(instance.ids_list) == 200
+
+    def test_get_ids_300(self):
+        instance = IdCollector()
+        instance.get_ids(300)
+        assert len(instance.ids_list) == 250
+
+#@pytest.mark.parametrize('param, result', )
+#@mock.patch()
